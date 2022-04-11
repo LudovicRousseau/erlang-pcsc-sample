ad2d1a06b6b7224c8ad3c408bbe9bb5fb3c19e74
Christopher Draeger <tofferoma@gmail.com>
2022-04-11 15:56:24 +0200

    runs poc code on startup
    when running via rebar3 run the app is started by calling blog_app:start
    the original PoC code was added to this function and prints a list
    of readers found.
    Hello world was replaced with a slightly more meaningful string
    
    Sample output without readers (empty list)
    
    :~/erlang-pcsc-sample/blog$ rebar3 run
    ===> Verifying dependencies...
    ===> Analyzing applications...
    ===> Compiling blog
    src/blog.erl:4:6: Warning: variable 'X' is unused
    
    ===> Assembling release blog-1.0...
    ===> Warnings generating release:
    *WARNING* Missing application sasl. Can not upgrade with this release
    ===> Release successfully assembled: _build/default/rel/blog
    [...]
    Erlang/OTP 24 [erts-12.3.1] [source] [64-bit] [smp:6:6] [ds:6:6:10] [async-threads:1] [jit]
    
    List of available readers:
    []

--- blog/src/blog_app.erl
+++ blog/src/blog_app.erl
@@ -10,6 +10,9 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    io:fwrite("List of available readers:\n"),
+    {ok, Readers} = pcsc_card_db:list_readers(),
+    erlang:display(Readers),
     blog_sup:start_link().
 
 stop(_State) ->
