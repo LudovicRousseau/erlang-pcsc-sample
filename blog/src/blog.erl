21f150c12afe39a15f800d8a6281e3e2c6a551a1
Ludovic Rousseau <ludovic.rousseau@free.fr>
2022-04-13 11:53:01 +0200

    Send the "select AID" APDU

--- blog/src/blog.erl
+++ blog/src/blog.erl
@@ -12,3 +12,13 @@ main() ->
 	% connect
 	{ok, Card} = pcsc_card:start_link(Reader, shared, [t1, t0]),
 
+	% select applet
+	Aid = << 160, 0, 0, 0, 98, 99, 1, 12, 6, 0 >>,
+	{ok, Replies} = pcsc_card:command(Card,
+	  {cla = iso, ins = select, p1 = 0, p2 = 10, data = Aid}),
+	io:fwrite(Replies).
+
+	% command
+	%{ok, Replies2} = pcsc_card:command(Card,
+	%	{cla = iso, ins = 0, p1 = 0, p2}),
+	%io:fwrite(Replies2).
