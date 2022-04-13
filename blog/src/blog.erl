c60d05e9b774527cdbe469d4e05623b85bba14c6
Ludovic Rousseau <ludovic.rousseau@free.fr>
2022-04-13 11:52:23 +0200

    connect to the first reader

--- blog/src/blog.erl
+++ blog/src/blog.erl
@@ -1,6 +1,14 @@
 -module(blog).
--export([main/1]).
+-export([main/0]).
 
-main(X) ->
-	io:fwrite("Hello world!\n"),
-	pcsc_card_db:list_readers().
+main() ->
+	% get all the available readers
+	{ok, Readers} = pcsc_card_db:list_readers(),
+
+	% use the first reader
+	[Reader | _] = Readers,
+	io:fwrite("Using: ~s~n", [Reader]),
+
+	% connect
+	{ok, Card} = pcsc_card:start_link(Reader, shared, [t1, t0]),
+
